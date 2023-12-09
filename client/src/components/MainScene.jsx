3d6d4012fdf5048ac0b68571eb9476d8e72637ab
Judit Calero <judit.calero84@gmail.com>
2023-12-10 00:17:03 +0100

    3d view kinda working and button functionality started to work

--- client/src/components/MainScene.jsx
+++ client/src/components/MainScene.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useRef, useState } from 'react'
 
-export default function MainScene() {
+export default function MainScene({setActiveSceneObject, buttonSelected}) {
 
     const [isClicked, setIsClicked] = useState(false)
     const [isHovered, setIsHovered] = useState(false)
@@ -10,10 +10,34 @@ export default function MainScene() {
     const Cube = ({ position, size, color}) => {
 
         const ref = useRef();
+        const name = "a regular cube";
 
-        function handleClick(){
+        function handleClick(name){
             setIsClicked(!isClicked)
+            if(buttonSelected !== "(press an action button)"){
+                if (buttonSelected === "Look At"){
+                    console.log("the most annoying cube that resets its rotation when I hover the mouse over it...")
+                }
+                else if(buttonSelected === "Open"){
+                    console.log("yeeee i'd love to open this cube wide open")
+                }
+                else{
+                    console.log("I don't uderstand what you want me to do")
+                }
+            }
+            
         }        
+
+        function handlePointerEnter(e){
+            event.stopPropagation()
+            setIsHovered(true)
+            setActiveSceneObject(name)
+        }
+
+        function handlePointerLeave(e){
+            setIsHovered(false)
+            setActiveSceneObject("")
+        }
 
         useFrame((state, delta) => {
             ref.current.rotation.x += delta / 2;
@@ -21,32 +45,29 @@ export default function MainScene() {
         })
 
         return (
-            <mesh   onClick={handleClick}
-                    onPointerEnter={(event) => {event.stopPropagation(), setIsHovered(true)}} 
-                    onPointerLeave={() => setIsHovered(false)}
+            <mesh   onClick={() => handleClick(name)}
+                    onPointerEnter={handlePointerEnter} 
+                    onPointerLeave={handlePointerLeave}
                     position={position} ref={ref}>
-                <boxGeometry args={isHovered ? [3, 3, 3] : [2, 2, 2]}/>
-                <meshStandardMaterial color={isHovered ? "orange" : "blue"}/>
+                    
+                <boxGeometry args={[2, 2, 2]}/>
+                <meshStandardMaterial color={isHovered ? "orange" : "white"}/>
             </mesh>
         )
     }
+
+    
 
   return (
     <div className="main_window">
         MainScene
         <Canvas>
-    
-        {/* <group>
-            <Cube position={[3, 0, 0]} color={"orange"}/>
-            <Cube position={[-3, 0, 0]} color={"green"}/>
-            <Cube position={[-3, 2, 0]} color={"red"}/>
-            <Cube position={[3, 2, 0]} color={"blue"}/>
-        </group> */}
 
-        <Cube position={[0, 0, 0]} />
+        <Cube name="a not so regular cube" position={[0, 0, 0]} />
 
-            <ambientLight intensity={0.2} />
+            <ambientLight intensity={0.1} />
             <directionalLight color="white" position={[0, 0, 5]} />
+
         </Canvas>
     </div>
   )
--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,30 +1,43 @@
 import React from 'react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import MainScene from '../components/MainScene'
 
 export default function main() {
+
+  //state:
+  const [buttonSelected, setButtonSelected] = useState("(press an action button)");
+  const [activeSceneObject, setActiveSceneObject] = useState("")
+
+
+  function handleMenuButtonClick(e){
+    e.preventDefault()
+    console.log(e.target.name)
+    setButtonSelected(e.target.name)
+  }
 
   return (
     <div className="main_app_body">
 
         <div>
 
-            <MainScene />
+            <MainScene buttonSelected={buttonSelected} setActiveSceneObject={setActiveSceneObject}/>
+
         </div>
 
-        <div>Aquí va la barra on es mostren els mouse hovers</div>
+        <div>{buttonSelected } 
+             {buttonSelected === "(press an action button)" ?  "" : " " + activeSceneObject}</div>
 
         <div className="main_two_columns">
             <div className="main_buttons_grid">
-                <div className="grid_item">Give</div>
-                <div className="grid_item">Pick Up</div>
-                <div className="grid_item">Use</div>
-                <div className="grid_item">Open</div>
-                <div className="grid_item">Look At</div>
-                <div className="grid_item">Push</div>
-                <div className="grid_item">Close</div>
-                <div className="grid_item">Talk To</div>
-                <div className="grid_item">Pull</div>
+                <button onClick={handleMenuButtonClick} name="Give" className="grid_item">Give</button>
+                <button onClick={handleMenuButtonClick} name="Pick Up" className="grid_item">Pick Up</button>
+                <button onClick={handleMenuButtonClick} name="Use" className="grid_item">Use</button>
+                <button onClick={handleMenuButtonClick} name="Open" className="grid_item">Open</button>
+                <button onClick={handleMenuButtonClick} name="Look At" className="grid_item">Look At</button>
+                <button onClick={handleMenuButtonClick} name="Push" className="grid_item">Push</button>
+                <button onClick={handleMenuButtonClick} name="Close" className="grid_item">Close</button>
+                <button onClick={handleMenuButtonClick} name="Talk To" className="grid_item">Talk To</button>
+                <button onClick={handleMenuButtonClick} name="Pull" className="grid_item">Pull</button>
      
             </div>
 
